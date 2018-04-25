817a2a42fd4832e2794c69e9b1bf6c6cac8df152
Banana Viking <bananavikingcoder@gmail.com>
2018-04-25 12:39:37 -0500

    Load words from text file.

--- WordScramble/ViewController.swift
+++ WordScramble/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import GameKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let startWordsPath = Bundle.main.path(forResource: "start", ofType: "txt") {
+            if let startWords = try? String(contentsOfFile: startWordsPath) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        } else {
+            allWords = ["silkworm"]
+        }
+        startGame()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func startGame() {
+        allWords = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: allWords) as! [String]
+        title = allWords[0]
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
 
 }
 
